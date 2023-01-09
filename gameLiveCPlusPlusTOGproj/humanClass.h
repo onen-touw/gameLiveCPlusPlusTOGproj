88914d9c54179a653ce46aa7f3251f8e118908b0
Даниил Гринченко <daniilgrinchenko20039@mail.ru>
2023-01-09 09:56:49 +0300

    +queue

--- gameLiveCPlusPlusTOGproj/humanClass.h
+++ gameLiveCPlusPlusTOGproj/humanClass.h
@@ -5,11 +5,22 @@
 class humanClass
 {
 private :
-	int hp = gameSettings::humanSetting.hp;
+	int hp = 0;
+	int stamina = 0;
+	int satiety = 0;
+	position humanPosition;
+
 public:
-	int getHp() { return this->hp; }
-	void setHp(int hp) {
-		this->hp = hp
+	humanClass(short int i, short int j)
+	{
+		this->humanPosition.i = i;
+		this->humanPosition.j = j;
+		this->hp = gameSettings::humanSetting.hp;
+		this->stamina = gameSettings::humanSetting.stamina;
+		this->satiety = gameSettings::humanSetting.satiety;
 	}
+	~humanClass() { }
+
+	int getHp() { return this->hp; }
 };
 
--- gameLiveCPlusPlusTOGproj/settings.h
+++ gameLiveCPlusPlusTOGproj/settings.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <string>
+
 struct humanSettings
 {
-	int hp = 10;
+	short int hp = 10;
+	short int stamina = 30;
+	short int satiety = 6;
 };
 
+struct position
+{
+	short int i = 0;
+	short int j = 0;
+};
 
 
 namespace gameSettings {
