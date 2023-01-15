846ae2b41c9520fa75c5165a13685008b17a3aa9
onen-touw <103072519+onen-touw@users.noreply.github.com>
2023-01-15 10:20:29 +0300

    fielfClass#3

--- gameLiveCPlusPlusTOGproj/main.cpp
+++ gameLiveCPlusPlusTOGproj/main.cpp
@@ -1,30 +1,9 @@
-
-//#include<SDL.h>
-//#include<SDL_image.h>
-//
-//#include <iostream>
-
 #include "settlmentActionsClass.h"	
 
 
 int main(int argc, char* argv[]) {
 
-	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-	/*baseGameClass baseGame;
-
-	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-	imageClass imageTest;
-	
-
-	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-	fieldClass fieldTest;*/
-
-
-
-	//fieldTest.blit(imagesNames::bushWithoutBerryCellHouseArea, 0, 0);
-
-
-	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
+	///TEST TEST TEST TEST 
 
 	settlmentActionsClass settlment;
 
