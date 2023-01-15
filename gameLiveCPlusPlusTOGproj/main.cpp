c5b1aa3a40a47eb9884dc9a452cbf75778bd7b01
onen-touw <103072519+onen-touw@users.noreply.github.com>
2023-01-15 08:50:09 +0300

    settlementAction#1.0

--- gameLiveCPlusPlusTOGproj/main.cpp
+++ gameLiveCPlusPlusTOGproj/main.cpp
@@ -1,26 +1,23 @@
 
-#include<SDL.h>
-#include<SDL_image.h>
+//#include<SDL.h>
+//#include<SDL_image.h>
+//
+//#include <iostream>
 
-#include <iostream>
-
-#include"baseGameClass.h"
-#include "fieldClass.h"
-#include"imageClass.h"
+#include "settlmentActionsClass.h"	
 
 
 int main(int argc, char* argv[]) {
 
 	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-	baseGameClass baseGame;
+	/*baseGameClass baseGame;
 
 	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
 	imageClass imageTest;
-	imageTest.loadImages();
-	imageTest.logOut();
+	
 
 	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-	fieldClass fieldTest;
+	fieldClass fieldTest;*/
 
 
 
@@ -29,88 +26,9 @@ int main(int argc, char* argv[]) {
 
 	///TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
 
-	if (baseGame.initModuls())
-	{
-		fieldTest.createFieldV();
-		fieldTest.generationObjects();
-		fieldTest.blitField();
-		SDL_UpdateWindowSurface(gameSettings::win);
+	settlmentActionsClass settlment;
 
-		std::cout << "ready\n";
-		SDL_Event event;
-		bool game = true;
-		bool firstHouse = false;
-		bool h = false;
-		int cursor_X = 0, cursor_Y = 0;
-
-		///test setHouse by Coords + test setPerson BY coords
-		/*fieldTest.setHouse({ 15,15 }, "");
-		fieldTest.setPersonCoors(5, 5, "t");
-		fieldTest.setPersonCoors(7, 7, "t");*/
-
-
-		while (game)
-		{
-			while (SDL_PollEvent(&event))
-			{
-				if (event.type == SDL_QUIT)//отслеживание закрытия окна через кнопку "Крест"
-				{
-					game = false;
-				}
-
-				else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RIGHT) {
-					fieldTest.transmitField(directions::right);
-				}
-				else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_UP)
-				{
-					fieldTest.transmitField(directions::up);
-				}
-				else if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_LEFT)
-				{
-					fieldTest.transmitField(directions::left);
-				}
-				else if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_DOWN)
-				{
-					fieldTest.transmitField(directions::down);
-				}
-
-				if (!firstHouse)
-				{
-					if (event.type == SDL_MOUSEMOTION)
-					{
-						fieldTest.blitField();
-						cursor_X = event.motion.x; cursor_Y = event.motion.y;
-						if (cursor_Y > gameSettings::winObjSize.menuHeader)
-						{
-							h = fieldTest.chosePositionForHouse({ (short int)cursor_X, (short int)cursor_Y });
-							SDL_UpdateWindowSurface(gameSettings::win);
-						}
-					}
-					if (event.button.button == SDL_BUTTON_LEFT && event.type == SDL_MOUSEBUTTONUP && h)
-					{
-						std::cout << "ok\n";
-						//SDL_GetMouseState(&cursor_X, &cursor_Y);
-						fieldTest.setHouse({ (short)cursor_X,(short)cursor_Y }, "px");
-						
-						fieldTest.blitField();
-						SDL_UpdateWindowSurface(gameSettings::win);
-						firstHouse = true;
-					}
-
-
-				}
-				
-				SDL_Delay(1000 / 60);
-			}
-		}
-	}
-	else
-	{
-		std::cout << "problemm\n";
-	}
-	
-
-	
+	settlment.start();
 
 	return 0;
 }
--- gameLiveCPlusPlusTOGproj/settlmentActionsClass.h
+++ gameLiveCPlusPlusTOGproj/settlmentActionsClass.h
@@ -1,5 +1,123 @@
 #pragma once
+
+#include"baseGameClass.h"
+#include "fieldClass.h"
+#include"imageClass.h"
+
 class settlmentActionsClass
 {
+private:
+	baseGameClass baseGame;
+	imageClass images;
+	fieldClass field;
+
+
+
+
+	bool game = true;
+	bool firstHouse = false;
+	bool goodForHouse = false;
+
+	int cursor_X = 0, cursor_Y = 0;
+
+public:
+
+	settlmentActionsClass()
+	{
+		images.loadImages();
+		images.logOut();
+
+		field.createFieldV();
+		field.generationObjects();
+
+
+		std::cout << "ready\n";
+		///bliting
+		field.blitField();
+		SDL_UpdateWindowSurface(gameSettings::win);
+	}
+	~settlmentActionsClass()
+	{
+
+	}
+
+
+	int start() {
+
+		if (baseGame.initModuls())
+		{
+			SDL_Event event;
+
+
+			///test setHouse by Coords + test setPerson BY coords
+			/*field.setHouse({ 15,15 }, "");
+			field.setPersonCoors(5, 5, "t");
+			field.setPersonCoors(7, 7, "t");*/
+
+
+			while (this->game)
+			{
+				while (SDL_PollEvent(&event))
+				{
+					if (event.type == SDL_QUIT)//отслеживание закрытия окна через кнопку "Крест"
+					{
+						this->game = false;
+					}
+
+					else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RIGHT) {
+						this->field.transmitField(directions::right);
+					}
+					else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_UP)
+					{
+						this->field.transmitField(directions::up);
+					}
+					else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_LEFT)
+					{
+						this->field.transmitField(directions::left);
+					}
+					else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_DOWN)
+					{
+						this->field.transmitField(directions::down);
+					}
+
+					if (!firstHouse)
+					{
+						if (event.type == SDL_MOUSEMOTION)
+						{
+							this->field.blitField();
+							cursor_X = event.motion.x; cursor_Y = event.motion.y;
+							if (cursor_Y > gameSettings::winObjSize.menuHeader)
+							{
+								this->goodForHouse = field.chosePositionForHouse({ (short int)cursor_X, (short int)cursor_Y });
+								SDL_UpdateWindowSurface(gameSettings::win);
+							}
+						}
+						if (event.button.button == SDL_BUTTON_LEFT && event.type == SDL_MOUSEBUTTONUP && goodForHouse)
+						{
+							field.setHouse({ (short)cursor_X,(short)cursor_Y }, "px");
+							field.blitField();
+							SDL_UpdateWindowSurface(gameSettings::win);
+
+							firstHouse = true;
+
+							std::cout << "ok\n";
+						}
+
+
+					}
+
+					SDL_Delay(1000 / 60);
+				}
+			}
+		}
+
+		else
+		{
+			std::cout << "problemm\n";
+		}
+
+
+		return 0;
+	}
 };
 
